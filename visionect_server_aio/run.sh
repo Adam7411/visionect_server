3a0817ec947794fa9443e6b9ed106888f6f08996
Adam7411 <101505511+Adam7411@users.noreply.github.com>
2025-08-14 12:46:36 +0200

    Create run.sh

---
+++ visionect_server_aio/run.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/with-contenv bash
+
+echo "********************************"
+echo "Congratulations!"
+echo "The build process is working."
+echo "This is just a test addon."
+echo "********************************"
